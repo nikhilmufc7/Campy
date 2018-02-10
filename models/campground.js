a624df8d44b1c4424526d83ecc5222921f4dd034
Nikhil Singh <Singh.nikhil999@gmail.com>
2018-02-10 21:47:40 +0530

    :penguin: new changes updated

--- models/campground.js
+++ models/campground.js
@@ -1,10 +1,15 @@
 var mongoose = require("mongoose");
+
 var campgroundSchema = new mongoose.Schema({
-	name : String,
-	image: String,
-	description: String
+   name: String,
+   image: String,
+   description: String,
+   comments: [
+      {
+         type: mongoose.Schema.Types.ObjectId,
+         ref: "Comment"
+      }
+   ]
 });
 
-var Campground =  mongoose.model("campground",campgroundSchema);
-
-module.exports =  mongoose.model("Campground",campgroundSchema);
+module.exports = mongoose.model("Campground", campgroundSchema);
