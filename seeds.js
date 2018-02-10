64b54e5129c3b36dd5d23f464daf157684c31d18
Nikhil Singh <Singh.nikhil999@gmail.com>
2018-02-10 21:44:31 +0530

    added stationary files for seeding database :fire:

---
+++ seeds.js
@@ -0,0 +1,58 @@
+var mongoose = require("mongoose");
+var Campground = require("./models/campground");
+var Comment   = require("./models/comment");
+
+var data = [
+    {
+        name: "Cloud's Rest",
+        image: "https://burst.shopifycdn.com/photos/campfire_373x@2x.jpg",
+        description: "blah blah blah"
+    },
+    {
+        name: "Desert Mesa",
+        image: "https://burst.shopifycdn.com/photos/relaxing-roadside-camping_373x@2x.jpg",
+        description: "blah blah blah"
+    },
+    {
+        name: "Canyon Floor",
+        image: "https://burst.shopifycdn.com/photos/camping-tents_373x@2x.jpg",
+        description: "blah blah blah"
+    }
+]
+
+function seedDB(){
+   //Remove all campgrounds
+   Campground.remove({}, function(err){
+        if(err){
+            console.log(err);
+        }
+        console.log("removed campgrounds!");
+         //add a few campgrounds
+        data.forEach(function(seed){
+            Campground.create(seed, function(err, campground){
+                if(err){
+                    console.log(err)
+                } else {
+                    console.log("added a campground");
+                    //create a comment
+                    Comment.create(
+                        {
+                            text: "This place is great, but I wish there was internet",
+                            author: "Homer"
+                        }, function(err, comment){
+                            if(err){
+                                console.log(err);
+                            } else {
+                                campground.comments.push(comment);
+                                campground.save();
+                                console.log("Created new comment");
+                            }
+                        });
+                }
+            });
+        });
+    });
+    //add a few comments
+}
+
+module.exports = seedDB;
