03274698d41572ada4254f5e37f978738f3534f6
Nikhil Singh <Singh.nikhil999@gmail.com>
2018-02-11 17:45:51 +0530

    linked external stylesheets

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var express     = require("express"),
 mongoose.connect("mongodb://localhost/yelp_camp_v4");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 seedDB();
 
 app.get("/", function(req, res){
