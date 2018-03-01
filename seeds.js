cacb55c46349cb9fb9af6f5e2f39a70366a415c8
Nikhil Singh <Singh.nikhil999@gmail.com>
2018-03-01 23:49:13 +0530

    minor

--- seeds.js
+++ seeds.js
@@ -4,54 +4,54 @@ var Comment   = require("./models/comment");
 
 var data = [
     {
-        name: "Cloud's Rest",
-        image: "https://burst.shopifycdn.com/photos/campfire_373x@2x.jpg",
-        description: "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth. Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar."
+        name: "Cloud's Rest", 
+        image: "https://farm4.staticflickr.com/3795/10131087094_c1c0a1c859.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
     },
     {
-        name: "Desert Mesa",
-        image: "https://burst.shopifycdn.com/photos/relaxing-roadside-camping_373x@2x.jpg",
-        description: "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth. Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar."
+        name: "Desert Mesa", 
+        image: "https://farm6.staticflickr.com/5487/11519019346_f66401b6c1.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
     },
     {
-        name: "Canyon Floor",
-        image: "https://burst.shopifycdn.com/photos/camping-tents_373x@2x.jpg",
-        description: "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth. Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar."
+        name: "Canyon Floor", 
+        image: "https://farm1.staticflickr.com/189/493046463_841a18169e.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
     }
 ]
 
 function seedDB(){
    //Remove all campgrounds
    Campground.remove({}, function(err){
-//         if(err){
-//             console.log(err);
-//         }
-//         console.log("removed campgrounds!");
-//          //add a few campgrounds
-//         data.forEach(function(seed){
-//             Campground.create(seed, function(err, campground){
-//                 if(err){
-//                     console.log(err)
-//                 } else {
-//                     console.log("added a campground");
-//                     //create a comment
-//                     Comment.create(
-//                         {
-//                             text: "This place is great, but I wish there was internet",
-//                             author: "Homer"
-//                         }, function(err, comment){
-//                             if(err){
-//                                 console.log(err);
-//                             } else {
-//                                 campground.comments.push(comment);
-//                                 campground.save();
-//                                 console.log("Created new comment");
-//                             }
-//                         });
-//                 }
-//             });
+        if(err){
+            console.log(err);
+        }
+        console.log("removed campgrounds!");
+         //add a few campgrounds
+        data.forEach(function(seed){
+            Campground.create(seed, function(err, campground){
+                if(err){
+                    console.log(err)
+                } else {
+                    console.log("added a campground");
+                    //create a comment
+                    Comment.create(
+                        {
+                            text: "This place is great, but I wish there was internet",
+                            author: "Homer"
+                        }, function(err, comment){
+                            if(err){
+                                console.log(err);
+                            } else {
+                                campground.comments.push(comment);
+                                campground.save();
+                                console.log("Created new comment");
+                            }
+                        });
+                }
+            });
         });
-
+    }); 
     //add a few comments
 }
 
