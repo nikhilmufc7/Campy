6a88c29f43a7611a809574789b7512190213596a
Nikhil Singh <Singh.nikhil999@gmail.com>
2018-03-02 23:54:38 +0530

    :rocket:

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express     = require("express"),
     bodyParser  = require("body-parser"),
     mongoose    = require("mongoose"),
     passport    = require("passport"),
+    methodOverride = require("method-override"),
     LocalStrategy = require("passport-local"),
     Campground  = require("./models/campground"),
     Comment     = require("./models/comment"),
@@ -18,7 +19,8 @@ mongoose.connect("mongodb://localhost/yelp_camp_v9");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
-// seedDB(); //seed the database
+app.use(methodOverride("_method"));
+seedDB(); //seed the database
 
 // PASSPORT CONFIGURATION
 app.use(require("express-session")({
