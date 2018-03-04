7f6b26b97d6e079b7a68ea1af56f50b44996742b
Nikhil Singh <Singh.nikhil999@gmail.com>
2018-03-04 22:58:45 +0530

    added delete routes :rocket:

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
-seedDB(); //seed the database
+// seedDB(); //seed the database
 
 // PASSPORT CONFIGURATION
 app.use(require("express-session")({
