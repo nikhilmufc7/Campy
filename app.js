b0786f1335231f81c5cf20562a12c98846e19978
Nikhil Singh <Singh.nikhil999@gmail.com>
2018-03-12 02:29:19 +0530

    minor update :rocket:

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index")
 
-var url = process.env.DATABASEURL || "mongodb://localhost/yelpcamp11"
+var url = process.env.DATABASEURL || "mongodb://localhost/campy"
 
 mongoose.connect(url);
 
